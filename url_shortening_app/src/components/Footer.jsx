70376ab0e0083f61ebbdf1085cac58219c84a94b
Melek Deniz Colak <deniz@Meleks-Air.hitronhub.home>
2023-09-30 10:39:20 -0400

    Update

--- url_shortening_app/src/components/Footer.jsx
+++ url_shortening_app/src/components/Footer.jsx
@@ -3,9 +3,32 @@ function Footer() {
  
 
     return (
-      <div className=" bg-slate-800">
-        <p></p> 
+      <div className=" bg-slate-800 text-slate-400 text-sm">
+        <div className="mr-44 ml-44 flex pt-14 pb-14 justify-between">
 
+          <p className=" text-2xl text-slate-100">Shortly</p>
+
+          <div className="flex flex-col">
+            <p className=" font-semibold text-slate-100 pb-4">Features</p>
+            <a href="" className="hover:text-cyan-600">Link Shortening</a>
+            <a href="" className="hover:text-cyan-600">Branded Links</a>
+            <a href="" className="hover:text-cyan-600">Analytics</a>
+          </div> 
+
+          <div className="flex flex-col">
+            <p className=" font-semibold text-slate-100 pb-4">Resources</p>
+            <a href="" className="hover:text-cyan-600">Blog</a>
+            <a href="" className="hover:text-cyan-600">Developers</a>
+            <a href="" className="hover:text-cyan-600">Support</a>
+          </div>
+
+          <div className="flex flex-col">
+            <p className=" font-semibold text-slate-100 pb-4">Company</p>
+            <a href="" className="hover:text-cyan-600">About</a>
+            <a href="" className="hover:text-cyan-600">Careers</a>
+            <a href="" className="hover:text-cyan-600">Contact</a>
+          </div>
+        </div>
         
         
       </div>
--- url_shortening_app/src/components/content/ShortenIt.jsx
+++ url_shortening_app/src/components/content/ShortenIt.jsx
@@ -1,5 +1,25 @@
-
+import {useState} from "react"
 function ShortenIt() {
+    const [allLinks, setAllLinks] = useState([]);
+    const [link, setLink] = useState({
+        inputLink: "",
+        shortLink: ""
+    });
+
+    function handleChange(event) {
+        setLink(prevLink => {
+            return {
+                ...prevLink,
+                [event.target.name]: event.target.value
+            }
+        })
+    }
+
+    console.log(link);
+
+    // function handleSubmit(){
+    //     setLink()
+    // }
  
 
     return (
@@ -7,7 +27,13 @@ function ShortenIt() {
 
             <form className="w-full mr-44 mt-10 p-10 bg-purple-950 bg-shorten-it flex justify-between items-center rounded-lg">
                 
-                <input className="w-full mr-10 h-12 p-5 rounded-lg" type="text" name="link" id="link" placeholder="Shorten a link here..." />
+                <input className="w-full mr-10 h-12 p-5 rounded-lg" 
+                    type="text" 
+                    name="inputLink"  
+                    value={link.inputLink}
+                    onChange={handleChange}
+                    placeholder="Shorten a link here..." 
+                />
                 <button className="bg-cyan-300 text-white font-bold pl-5 pr-5 p-2 rounded-lg text-base w-40 h-12">Shorten It!</button>
             </form>
 
